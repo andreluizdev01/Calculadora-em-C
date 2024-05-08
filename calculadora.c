a48e6725e40b44f736d49d4700c71f5ccd997211
André Luiz <142675217+andreluizdev01@users.noreply.github.com>
2024-05-08 16:05:44 -0300

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int num1, num2, soma, sub, div, multi;
+    
+    printf("Caluladora do \"framengo\" 1.0 \n");
+    printf("entre os 2 numeros a serem processados \n");
+    scanf("%i%i", &num1, &num2);
+    
+    soma = num1 + num2;
+    sub = num1 - num2;
+    div = num1 / num2;
+    multi = num1 * num2;
+
+    printf( "A soma e:  %i\n", soma);
+    printf( "A sbutração e; %i \n", sub);
+    printf( "A divisao é; %i\n", div);
+    printf( "A multiplicao é: %i\n", multi);
+    
+    return 0;
+}
